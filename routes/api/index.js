3008bad513959dcc0afb36bd14517bb53a44b9fc
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-13 14:26:09 -0700

    basic routes added and server file

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+//import all of the api routes to prefix their endpoint names and package them 
+
+const router = require('express').Router();
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+//add prefix of /pizzas to routes created in pizza-routes
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
